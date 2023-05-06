d3f2f9ebba85043ed6e53b22f8d0e9813ace8e50
juno-choi <ililil9482@naver.com>
2023-05-06 23:36:40 +0900

    [추가] 반환을 위한 스키마 파일 추가

--- backend/domain/question/question_router.py
+++ backend/domain/question/question_router.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from database import get_db
+from domain.question import question_schema
 from models import Question
 
 router = APIRouter(
     prefix="/v1/question",
 )
 
-@router.get("/list")
+@router.get("/list", response_model=list[question_schema.Question])
 def list(db: Session = Depends(get_db)):
     _question_list = db.query(Question).order_by(Question.create_at.desc()).all()
     return _question_list
