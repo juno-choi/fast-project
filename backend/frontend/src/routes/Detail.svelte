4c596928608e46c3a4995be3fdb6e61dd458c5bf
juno-choi <ililil9482@naver.com>
2023-05-07 18:07:02 +0900

    [추가] 상세 화면

--- backend/frontend/src/routes/Detail.svelte
+++ backend/frontend/src/routes/Detail.svelte
@@ -1,10 +1,20 @@
 <script>
     export let params = {}
     let question_id = params.question_id
-    console.log('question_id:'+ question_id)
+    let question = {}
+
+    function get_question() {
+        fetch("http://127.0.0.1:8000/v1/question/detail/"+question_id).then((res) => {
+            res.json().then((json) => {
+                question = json
+            })
+        });
+    }
+
+    get_question()
 </script>
 
-<h1>제목</h1>
+<h1>{question.subject}</h1>
 <div>
-    내용
+    {question.content}
 </div>
